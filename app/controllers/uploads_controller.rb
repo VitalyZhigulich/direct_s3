f7c7664d080f944383844b7bef24b3171ef15e82
Vitaly Zhigulich <vitko2003@gmail.com>
2021-07-15 01:58:22 +0300

    t

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,2 +1,11 @@
 class UploadsController < ApplicationController
+  def create
+    blob_signed_ids = params[:files]
+    purpose = ActiveStorage::Blob.combine_signed_id_purposes(:blob_id)
+    blob_ids = blob_signed_ids.map do |signed_id|
+      ActiveStorage::Blob.signed_id_verifier.verified(signed_id, purpose: purpose)
+    end
+
+    @blobs = ActiveStorage::Blob.where(id: blob_ids)
+  end
 end
